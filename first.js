8c43a0d8d6a564c8a4c735ad4816fb541e834b3c
Aashiq singh <108392697+Aashiq6354@users.noreply.github.com>
2024-01-26 14:48:43 +0530

    Add files via upload
    Basic

---
+++ first.js
@@ -0,0 +1,25 @@
+// console.log("Hello world!!")
+// console.warn("Hello world!!")
+// console.error("This is error..")
+// console.info("Hii this is royal")
+
+
+// number string boolean undefined
+
+var a = 100000000
+console.log(a);
+
+var x = 98.997
+console.log(x);
+
+var isActive = true 
+console.log(isActive);
+console.log(typeof(isActive));
+
+var y = 
+console.log("value of y = ",y);
+console.log(typeof(y));
+
+var z = "Royal technoosft"
+console.log(z);
+console.log(typeof(z));
