bc6b5c2eb5a3ed1937413dae0a0ebcb34ecf7ac8
Aashiq singh <108392697+Aashiq6354@users.noreply.github.com>
2024-02-16 14:44:30 +0530

    Add files via upload

---
+++ String1.js
@@ -0,0 +1,62 @@
+
+// console.log(str.length);
+
+// console.log(str.charAt(3))
+
+// var x = str.repeat(2)
+// console.log(x);
+
+
+// console.log(str.concat(" Person"))
+// console.log(str , "Person");
+
+
+// var x = str.charCodeAt(0)
+// console.log(str.charCodeAt(1));
+
+// var x = str.endsWith("te.")
+// console.log(x);
+
+
+// console.log(str.includes("is"))
+// console.log(str.includes("Royal"))
+
+// console.log(str.indexOf("am"))
+
+
+// console.log(str.lastIndexOf("a"))
+
+
+// var x = str.replace("am","is")
+// console.log(x);
+
+// console.log(str.search("a"))
+
+// var x = str.slice(3)
+// console.log(x);
+
+// var x = str.slice(8,13);
+// console.log(x);
+
+// var x = str.slice(-9 , 13)
+// console.log(x);
+
+// var x = str.split("a")
+// console.log(x);
+
+// var z = str.startsWith("A")
+// console.log(z);
+
+// console.log(str.substring(-1))
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// console.log(str.trim());
+// var str = "   AB am a Royalite.   "
+// console.log(str.length);
+// var x = str.trim()
+
+// console.log(x);
+// console.log(x.length);
+
